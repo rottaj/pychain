204686177d380b677734083bbceb7c27c4af6091
rottaj <jackrotta12@gmail.com>
2020-11-25 20:25:57 -0500

    added random generator.. is this how you blockchain?

--- block.py
+++ block.py
@@ -3,5 +3,7 @@ class Block:
         self.index = index
         self.value = value
 
+        
+
 
 
--- chain.py
+++ chain.py
@@ -1,3 +1,6 @@
+from pychain.block import Block
+import random
+
 class BlockChain:
     def __init__(self):
         self.chain = []
@@ -11,5 +14,10 @@ class BlockChain:
     def get_block(self, index):
         print('Block at index: ' + str(index) + str(self.chain[index]))
         print('Block Value: ', self.chain[index].value)
+
+    def create_random(self, size):
+        for i in range(size):
+            b = Block(i, random.randint(-500, 500)) # creates random value for block (-500, 500)
+            self.chain.append(b)
 
 
